f0d824a4385b0ce912bc91e25f0f4dd6b7e92a2a
SeysT <thibaut.seys@hotmail.fr>
2018-02-01 15:21:26 +0100

    document crossword_solver.py

--- crossword_solver.py
+++ crossword_solver.py
@@ -1,3 +1,26 @@
+"""
+Author: Thibaut Seys
+Last modified: 01/02/2018
+
+This file modelize and solve our crosswords problem.
+
+Usages:
+    crossword_solver.py <crossword_filename> <words_filename> [<maintain_arc>]
+
+Arguments:
+    crossword_filename: [Mandatory] the path of the crossword grid file
+    words_filename: [Mandatory] the path of the words file
+    maintain_arc: [Optional] whatever you put here will maintain arc consistency
+
+Model:
+    + variables: We have to different types of variables:
+        - the 'case' variables which correspond to cases to complete in the grid. Their domain is
+        composed by letters of the alphabet problem.
+        - the 'segment' variables which correspond to a segment to complete in the grid. Their
+        domain is composed with words of the same length.
+    + constraints: Our constraints link the two different types of variables. A 'case' variable
+    has to have the same letter than the one of the word in the segment.
+"""
 from sys import argv
 from time import time
 
